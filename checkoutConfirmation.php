28257caaf0413f84eaeec1fde9225e905e8b2792
aykenlee <73160980+aykenlee@users.noreply.github.com>
2023-01-28 20:37:12 +0800

    Add checkout confirmation page

---
+++ checkoutConfirmation.php
@@ -0,0 +1,26 @@
+<?php 
+// Include the code that contains shopping cart's functions.
+// Current session is detected in "cartFunctions.php, hence need not start session here.
+include_once("cartFunction.php");
+include("header.php"); // Include the Page Layout header
+
+if (! isset($_SESSION["ShopperID"])) { // Check if user logged in 
+	// redirect to login page if the session variable shopperid is not set
+	header ("Location: login.php");
+	exit;
+}
+
+echo "<div id='myShopCart' style='margin:auto'>"; // Start a container
+if (isset($_SESSION["Cart"])) {
+	include_once("mysql_conn.php");
+	
+
+    
+	$conn->close(); // Close database connection
+}
+else {
+	echo "<h3 style='text-align:center; color:red;'>Empty shopping cart!</h3>";
+}
+echo "</div>"; // End of container
+include("footer.php"); // Include the Page Layout footer
+?>
