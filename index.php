c3f0cf2b9c7dc03ab6dbcad00917d10f6a9f6152
Venturion18 <59863362+Venturion18@users.noreply.github.com>
2023-01-29 22:24:59 +0800

    Added promotion item display

--- index.php
+++ index.php
@@ -3,29 +3,8 @@
 session_start();
 // Include the Page Layout header
 include("header.php"); 
-?>
-<!-- <img src="Images/welcome2mamaya.jpg" class="img-fluid" 
-     style="display:block; margin:auto;"/>"; -->
 
-     <div class="product-slider">
-  <div id="carousel" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="item active"> <img src="Images/Slider_Index/1.png"> </div>
-      <div class="item"> <img src="Images/Slider_Index/2.png"> </div>
-      <div class="item"> <img src="Images/Slider_Index/3.png"> </div>
-      <div class="item"> <img src="Images/Slider_Index/4.png"> </div>
-      <div class="item"> <img src="Images/Slider_Index/5.png"> </div>
-      <!-- <div class="item"> <img src="http://placehold.it/1600x700?text=Product+06"> </div>
-      <div class="item"> <img src="http://placehold.it/1600x700?text=Product+07"> </div>
-      <div class="item"> <img src="http://placehold.it/1600x700?text=Product+08"> </div>
-      <div class="item"> <img src="http://placehold.it/1600x700?text=Product+09"> </div>
-      <div class="item"> <img src="http://placehold.it/1600x700?text=Product+10"> </div> -->
-    </div>
-  </div>
-</div>
-
-
-<?php 
+include("promotion.php");
 // Include the Page Layout footer
 include("footer.php"); 
 ?>
